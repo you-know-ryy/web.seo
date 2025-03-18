46772fd54b3eab05077cc866ecf8c4d6be7c165e
you-know-ryy <m.deriyansyahit@gmail.com>
2025-03-18 21:12:26 +0700

    Initial commit from gatsby: (https://github.com/decaporg/gatsby-starter-decap-cms.git)

---
+++ src/cms/cms.js
@@ -0,0 +1,16 @@
+import CMS from 'decap-cms-app'
+import uploadcare from 'decap-cms-media-library-uploadcare'
+import cloudinary from 'decap-cms-media-library-cloudinary'
+
+import AboutPagePreview from './preview-templates/AboutPagePreview'
+import BlogPostPreview from './preview-templates/BlogPostPreview'
+import ProductPagePreview from './preview-templates/ProductPagePreview'
+import IndexPagePreview from './preview-templates/IndexPagePreview'
+
+CMS.registerMediaLibrary(uploadcare)
+CMS.registerMediaLibrary(cloudinary)
+
+CMS.registerPreviewTemplate('index', IndexPagePreview)
+CMS.registerPreviewTemplate('about', AboutPagePreview)
+CMS.registerPreviewTemplate('products', ProductPagePreview)
+CMS.registerPreviewTemplate('blog', BlogPostPreview)
